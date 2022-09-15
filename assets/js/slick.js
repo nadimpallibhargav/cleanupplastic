5e8a0dcb42750e9b47040717e7d322f464b5c240
bhargav27b <bhargav@sketchbrahma.com>
2022-09-16 00:17:45 +0530

    about, form

--- assets/js/slick.js
+++ assets/js/slick.js
@@ -1,3 +1,22 @@
+if ($(".association-slider").length) {
+  var $newslider = $(".association-slider")
+    .on("init", function (slick) {
+      $(".association-slider").fadeIn(1000);
+    })
+    .slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      lazyLoad: "ondemand",
+      autoplaySpeed: 3500,
+      asNavFor: ".product__slider-thmb",
+      pauseOnHover: false,
+    });
+}
+
+
+
+
 if ($(".product__slider-main").length) {
   var $slider = $(".product__slider-main")
     .on("init", function (slick) {
@@ -60,76 +79,5 @@ if ($(".product__slider-main").length) {
     $(".product__slider-thmb .slick-current").addClass("slick-active1");
   } else {
     $(".product__slider-thmb .slick-current").removeClass("slick-active1");
-  }
-}
-
-// Differentiator Slider
-
-if ($(".differentiator-slider-main").length) {
-  var $slider = $(".differentiator-slider-main")
-    .on("init", function (slick) {
-      $(".differentiator-slider-main").fadeIn(1000);
-    })
-    .slick({
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      arrows: false,
-      autoplay: true,
-      lazyLoad: "ondemand",
-      autoplaySpeed: 3000,
-      vertical: true,
-      fade: true,
-      pauseOnHover: false,
-      asNavFor: ".differentiator-slider-thmb",
-    });
-
-  var $slider2 = $(".differentiator-slider-thmb")
-    .on("init", function (slick) {
-      $(".differentiator-slider-thmb").fadeIn(1000);
-    })
-    .slick({
-      slidesToShow: 6,
-      slidesToScroll: 1,
-      lazyLoad: "ondemand",
-      asNavFor: ".differentiator-slider-main",
-      dots: false,
-      centerMode: false,
-      focusOnSelect: true,
-      vertical: true,
-    });
-
-  //remove active class from all thumbnail slides
-  $(".differentiator-slider-thmb .slick-slide").removeClass("slick-active");
-
-  //set active class to first thumbnail slides
-  $(".differentiator-slider-thmb .slick-slide").eq(0).addClass("slick-active");
-
-  // On before slide change match active thumbnail to current slide
-  $(".differentiator-slider-main").on(
-    "beforeChange",
-    function (event, slick, currentSlide, nextSlide) {
-      var mySlideNumber = nextSlide;
-      $(".differentiator-slider-thmb .slick-slide").removeClass("slick-active");
-      $(".differentiator-slider-thmb .slick-slide").removeClass(
-        "slick-active1"
-      );
-      $(".differentiator-slider-thmb .slick-slide")
-        .eq(mySlideNumber)
-        .addClass("slick-active");
-      $(".differentiator-slider-thmb .slick-slide")
-        .eq(mySlideNumber)
-        .addClass("slick-active1");
-    }
-  );
-  function dthumbClick(obj) {
-    $(".differentiator-slider-thmb .slick-slide").removeClass("slick-active1");
-    $(obj).addClass("slick-active1");
-  }
-  if ($(".differentiator-slider-thmb .slick-slide").hasClass("slick-current")) {
-    $(".differentiator-slider-thmb .slick-current").addClass("slick-active1");
-  } else {
-    $(".differentiator-slider-thmb .slick-current").removeClass(
-      "slick-active1"
-    );
   }
 }
